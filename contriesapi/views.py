be6e493f282b3f5b9f64da77947b70b98ce78b4e
Mayeen4536 <63101853+Mayeen4536@users.noreply.github.com>
2022-10-13 23:57:12 +0600

    countries_detail view added

--- contriesapi/views.py
+++ contriesapi/views.py
@@ -36,3 +36,36 @@ def countries_list(request):
 
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
+
+def countries_detail(request, pk):
+    try:
+       
+        countries = Contries.objects.get(pk=pk)
+    except Contries.DoesNotExist:
+        return JsonResponse({'message': 'The country does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        countries_serializer = ContriesSerializer(countries)
+        return JsonResponse(countries_serializer.data)
+
+    elif request.method == 'PUT':
+        countries_data = JSONParser().parse(request)
+        countries_serializer = ContriesSerializer(countries, data=countries_data)
+
+        if countries_serializer.is_valid():
+            countries_serializer.save()
+            return JsonResponse(countries_serializer.data)
+        return JsonResponse(countries_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    elif request.method == 'DELETE':
+        countries.delete()
+        return JsonResponse({'message': 'Country was deleted Successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
