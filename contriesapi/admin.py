425be6126bde2a72800e89e0176c1a1bc4290f6a
Mayeen4536 <63101853+Mayeen4536@users.noreply.github.com>
2022-10-14 01:40:12 +0600

    created model objects

--- contriesapi/admin.py
+++ contriesapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Contries
+
 # Register your models here.
+
+admin.site.register(Contries)
--- contriesapi/models.py
+++ contriesapi/models.py
@@ -6,6 +6,9 @@ class Contries(models.Model):
     name = models.CharField(max_length=50, blank=False, default='')
     capital = models.CharField(max_length=50, blank=False, default='')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ('id',)
 
