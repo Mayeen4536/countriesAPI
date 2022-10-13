0b079bf795afa2d81c3b4df1e9bc8f0511e61335
Mayeen4536 <63101853+Mayeen4536@users.noreply.github.com>
2022-10-13 22:41:42 +0600

    created views countries_list

--- contriesapi/views.py
+++ contriesapi/views.py
@@ -13,6 +13,26 @@ from contriesapi.serializers import ContriesSerializer
 @api_view(['GET', 'POST'])
 
 def countries_list(request):
+    if request.method == 'GET':
+        countries = Contries.objects.all()
+
+        name = request.GET.get('name', None)
+        if name is not None:
+            countries = countries.filter(name_icontains=name)
+
+        countries_serializer = ContriesSerializer(countries, many=True)
+        return JsonResponse(countries_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        countries_data = JSONParser().parse(request)
+        countries_serializer = ContriesSerializer(data=countries_data)
+
+        if countries_serializer.is_valid():
+            countries_serializer.save()
+            return JsonResponse(countries_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(countries_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 
